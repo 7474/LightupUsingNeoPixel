0fbc7da99245eda7a134d302163cefb4d5e5815f
koudenpa <koudenpa@hotmail.com>
2016-09-07 04:06:07 +0900

    ESP8266自身がAPかつWebサーバとなって色の変更を行えるようにした。

--- LightupUsingNeoPixelStandAlone/LightupUsingNeoPixelStandAlone.ino
+++ LightupUsingNeoPixelStandAlone/LightupUsingNeoPixelStandAlone.ino
@@ -3,6 +3,10 @@
 #include <avr/power.h>
 #endif
 
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+
 #define LEDCNT 4
 #define PIN 0
 #define WAIT 50
@@ -18,6 +22,38 @@ class MyColor {
       g = _g;
       b = _b;
       a = _a;
+    }
+    MyColor(const char* colorCode) {
+      Serial.print(colorCode);
+      Serial.print("->");
+      if (strlen(colorCode) < 7 || colorCode[0] != '#') {
+        // XXX error
+        return;
+      }
+      r = hexToInt(&colorCode[1]);
+      g = hexToInt(&colorCode[3]);
+      b = hexToInt(&colorCode[5]);
+      if (strlen(colorCode) >= 9) {
+        // XXX alpha
+        a = hexToInt(&colorCode[7]);
+      } else {
+        a = 255;
+      }
+      Serial.print("(");
+      Serial.print(r, HEX);
+      Serial.print(", ");
+      Serial.print(g, HEX);
+      Serial.print(", ");
+      Serial.print(b, HEX);
+      Serial.print(", ");
+      Serial.print(a, HEX);
+      Serial.println(")");
+    }
+    static byte hexToInt(const char* hex) {
+      return (hexToInt(hex[0]) << 4) + hexToInt(hex[1]);
+    }
+    static byte hexToInt(const char hex) {
+      return hex >= 'a' ? hex - 'a' + 10 : hex - '0';
     }
 };
 
@@ -54,6 +90,10 @@ class MyFullColorLedStrip {
     virtual void updateOneCycle();
     int numPixels();
     void updateStrip();
+    //
+    void setFrameWait(int _frameWait) {
+      frameWait = _frameWait;
+    }
   protected:
     Adafruit_NeoPixel* pStrip;
     MyColor* backColors;
@@ -71,7 +111,7 @@ enum AuraMode {
 
 class AuraStrip : public MyFullColorLedStrip {
   public:
-    AuraStrip(Adafruit_NeoPixel* _pStrip) : MyFullColorLedStrip(_pStrip){
+    AuraStrip(Adafruit_NeoPixel* _pStrip) : MyFullColorLedStrip(_pStrip) {
     }
     void set(AuraMode _auraMode, MyColor _baseColor, MyColor _flashColor) {
       auraMode = _auraMode;
@@ -154,9 +194,9 @@ void MyFullColorLedStrip::updateStrip() {
   for (int i = 0; i < numPixels(); i++) {
     MyColor c;
     MyColor* pc = &c;
-//    Serial.print("fore#a =  ");
-//    Serial.print(foreColors[i].a, HEX);
-//    Serial.print(": ");
+    //    Serial.print("fore#a =  ");
+    //    Serial.print(foreColors[i].a, HEX);
+    //    Serial.print(": ");
     if (foreColors[i].a == 255) {
       pc = &foreColors[i];
     } else if (foreColors[i].a == 0) {
@@ -168,22 +208,27 @@ void MyFullColorLedStrip::updateStrip() {
       pc->g =  ((pc1->g * (255 - pc2->a)) >> 8) + ((pc2->g * (int)pc2->a) >> 8);
       pc->b =  ((pc1->b * (255 - pc2->a)) >> 8) + ((pc2->b * (int)pc2->a) >> 8);
     }
-//    Serial.print("(");
-//    Serial.print(pc->r, HEX);
-//    Serial.print(", ");
-//    Serial.print(pc->g, HEX);
-//    Serial.print(", ");
-//    Serial.print(pc->b, HEX);
-//    Serial.print(", ");
-//    Serial.print(pc->a, HEX);
-//    Serial.println(")");
+    //    Serial.print("(");
+    //    Serial.print(pc->r, HEX);
+    //    Serial.print(", ");
+    //    Serial.print(pc->g, HEX);
+    //    Serial.print(", ");
+    //    Serial.print(pc->b, HEX);
+    //    Serial.print(", ");
+    //    Serial.print(pc->a, HEX);
+    //    Serial.println(")");
     pStrip->setPixelColor(i, pc->r, pc->g, pc->b);
   }
   pStrip->show();
 }
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(LEDCNT, PIN, NEO_GRB + NEO_KHZ800);
-MyFullColorLedStrip* pMyStrip;
+MyFullColorLedStrip* pMyStrip = NULL;
+MyFullColorLedStrip* pMyNextStrip = NULL;
+
+const char *ssid = "MyStrip";
+const char *password = "";
+ESP8266WebServer server(80);
 
 void setup() {
   // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
@@ -195,6 +240,8 @@ void setup() {
   // For loging
   Serial.begin(9600);
 
+  initializeServer();
+
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
   pMyStrip = new MyFullColorLedStrip(&strip);
@@ -203,9 +250,9 @@ void setup() {
 }
 
 void loop() {
-  Serial.println("Start loop.");
+  //  Serial.println("Start loop.");
   //  Serial.println(++loopCount, DEC);
-
+  server.handleClient();
   selectStrip();
 
   pMyStrip->updateOneCycle();
@@ -217,12 +264,68 @@ void loop() {
 }
 
 void selectStrip() {
-  // TODO 入力の割り込みの有無を見る
   // 入力があった場合
-  // TODO strip を入力に応じて更新する
-  delete  pMyStrip;
-  AuraStrip* pAuraStrip = new AuraStrip(&strip);
-  pAuraStrip->set(StartToEnd, MyColor(0, 127, 64), MyColor(192, 248, 224));
-  pMyStrip = pAuraStrip;
+  if (pMyNextStrip) {
+    // strip を入力に応じて更新する
+    delete  pMyStrip;
+    pMyStrip = pMyNextStrip;
+    pMyNextStrip = NULL;
+  }
+}
+
+void initializeServer() {
+  WiFi.softAP(ssid, password);
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(myIP);
+
+  server.on("/", handleRoot);
+  server.begin();
+}
+
+const char* form = "<html>"
+                   "<head>"
+                   "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">"
+                   "<style>input { display: block; margin: 4px; }</style>"
+                   "</head>"
+                   "<body>"
+                   "<form method=\"post\">"
+                   "<input type=\"color\" name=\"basecolor\" value=\"#008040\" >"
+                   "<input type=\"color\" name=\"flashcoolor\" value=\"#c0f0e0\" >"
+                   "<input type=\"number\" name=\"delay\" value=\"50\" >"
+                   "<input type=\"submit\" value=\"Send\" >"
+                   "</form>"
+                   "</body>"
+                   "</html>"
+                   ;
+
+void handleRoot() {
+  Serial.println("handleRoot");
+  if (server.method() != HTTP_GET) {
+    String baseColor = "#008040";
+    String flashColor = "#c0f0e0";
+    int frameWait = 50;
+
+    for ( uint8_t i = 0; i < server.args(); i++ ) {
+      //      Serial.print(server.argName(i));
+      //      Serial.print("=");
+      //      Serial.print(server.arg(i));
+      if (server.argName(i)[0] == 'b') {
+        baseColor = server.arg(i);
+        // baseColor.toLower();
+      }
+      if (server.argName(i)[0] == 'f') {
+        flashColor = server.arg(i);
+      }
+      if (server.argName(i)[0] == 'd') {
+        frameWait = server.arg(i).toInt();
+      }
+    }
+    AuraStrip* pAuraStrip = new AuraStrip(&strip);
+    pAuraStrip->set(StartToEnd, MyColor(baseColor.c_str()), MyColor(flashColor.c_str()));
+    pAuraStrip->setFrameWait(frameWait);
+    pMyNextStrip = pAuraStrip;
+  }
+  server.send(200, "text/html", form);
 }
 
